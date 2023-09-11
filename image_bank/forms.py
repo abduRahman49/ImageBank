29478f7c225816ee1c4acbfd18795144790f82c4
abduRahman94 <sabdouseye@gmail.com>
2023-09-11 22:12:31 +0000

    Correction bug - affichage champ prix pour image

--- image_bank/forms.py
+++ image_bank/forms.py
@@ -18,18 +18,6 @@ class ImageForm(forms.ModelForm):
         }
 
 
-# class AppSignupForm(SignupForm):
-#     def save(self, request):
-
-#         # Ensure you call the parent class's save.
-#         # .save() returns a User object.
-#         user = super(AppSignupForm, self).save(request)
-
-#         # Add your own processing here.
-
-#         # You must return the original result.
-#         return user
-
 class NewUserForm(forms.Form):
     username = forms.CharField(
         label='Username',
