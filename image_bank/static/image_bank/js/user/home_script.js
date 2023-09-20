c11b1941eff1212b2e484aa7252b23bc5973d50d
abduRahman94 <sabdouseye@gmail.com>
2023-09-20 18:45:53 +0000

    Gestion responsivité pour accueil utilisateur

--- image_bank/static/image_bank/js/user/home_script.js
+++ image_bank/static/image_bank/js/user/home_script.js
@@ -1,16 +1,3 @@
-
-
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', (e) => {
-      // Remove 'active' class from all links
-      document.querySelectorAll('.nav-link').forEach(otherLink => {
-        otherLink.classList.remove('active');
-      });
-  
-      // Add 'active' class to the clicked link
-      e.target.classList.add('active');
-    });
-});
   
 document.querySelector('#iconSidenav').addEventListener('click', (e) => {
     console.log('button clicked!')
@@ -20,9 +7,3 @@ document.querySelector('#iconSidenav').addEventListener('click', (e) => {
 document.querySelector('#iconNavbarSidenav').addEventListener('click', (e) => {
     document.querySelector('body').classList.toggle('g-sidenav-pinned');
 })
-
-// document.querySelector('#iconNavbarSidenav').addEventListener('click', (e) => {
-//   console.log('burger button clicked!')
-//   console.log(document.querySelector('body'))
-//     // document.querySelector('body').classList.toggle('g-sidenav-pinned');
-//   console.log(document.querySelector('body').classList.contains('g-sidenav-pinned'));
